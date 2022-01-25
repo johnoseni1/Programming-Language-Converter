04f942046763bda6d0ed78d2f81443709a9b05a4
John Oseni <johnoseni26@gmail.com>
2022-01-25 17:59:46 +0100

    fixed main-file.js

--- assets/js/main-file.js
+++ assets/js/main-file.js
@@ -66,20 +66,19 @@ $(window).on('load', function () {
 			itemNav: 'forceCentered',
 			activateMiddle: 1,
 			smart: 1,
-			activateOn: 'click',
-			//mouseDragging: 1,
-			touchDragging: 1,
-			releaseSwing: 1,
-			startAt: 10,
-			scrollBar: $scrollbar,
-			//scrollBy: 1,
-			activatePageOn: 'click',
 			speed: 200,
 			moveBy: 600,
 			elasticBounds: 1,
 			dragHandle: 1,
 			dynamicHandle: 1,
 			clickBar: 1,
+			activateOn: 'click',
+			touchDragging: 1,
+			releaseSwing: 1,
+			startAt: 10,
+			scrollBar: $scrollbar,
+			activatePageOn: 'click',
+
 		}).init();
 	}
 
